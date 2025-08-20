ae826eca868774d734101e563dfd463f528f0e52
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2025-08-20 20:16:52 -0300

    refactor: use RadioGroup in recurrence picker

--- lib/widgets/recurrence_picker.dart
+++ lib/widgets/recurrence_picker.dart
@@ -114,197 +114,185 @@ class _RecurrencePickerState extends State<RecurrencePicker> {
   Widget build(BuildContext context) {
     final locale = Localizations.localeOf(context).languageCode;
     final theme = Theme.of(context);
-    return IntrinsicWidth(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            context.tr("recurrence"),
-            style: theme.textTheme.titleSmall,
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(context.tr("repeat_every")),
-              const SizedBox(width: 10),
-              SizedBox(
-                width: 40,
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  controller: intervalController,
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly,
-                  ],
+    return RadioGroup<_EndOption>(
+      onChanged: (value) => setState(() {
+        endOption = value ?? _EndOption.never;
+      }),
+      groupValue: endOption,
+      child: IntrinsicWidth(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              context.tr("recurrence"),
+              style: theme.textTheme.titleSmall,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(context.tr("repeat_every")),
+                const SizedBox(width: 10),
+                SizedBox(
+                  width: 40,
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    controller: intervalController,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(width: 10),
-              DropdownMenu(
-                requestFocusOnTap: false,
-                initialSelection: frequency,
-                onSelected: (value) {
-                  setState(() {
-                    frequency = value ?? Frequency.daily;
-                  });
-                },
-                dropdownMenuEntries: [
-                  DropdownMenuEntry(
-                    value: Frequency.hourly,
-                    label: context.tr("repeat_menu.hour"),
-                  ),
-                  DropdownMenuEntry(
-                    value: Frequency.daily,
-                    label: context.tr("repeat_menu.day"),
-                  ),
-                  DropdownMenuEntry(
-                    value: Frequency.weekly,
-                    label: context.tr("repeat_menu.week"),
-                  ),
-                  DropdownMenuEntry(
-                    value: Frequency.monthly,
-                    label: context.tr("repeat_menu.month"),
-                  ),
-                  DropdownMenuEntry(
-                    value: Frequency.yearly,
-                    label: context.tr("repeat_menu.year"),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          ...switch (frequency) {
-            Frequency.weekly => [
-                Text(context.tr("repeat")),
-                SegmentedButton(
-                  showSelectedIcon: false,
-                  multiSelectionEnabled: true,
-                  expandedInsets: const EdgeInsets.symmetric(vertical: 10),
-                  segments: [
-                    for (final (day, string) in const [
-                      (DateTime.sunday, "weekday_letter.sunday"),
-                      (DateTime.monday, "weekday_letter.monday"),
-                      (DateTime.tuesday, "weekday_letter.tuesday"),
-                      (DateTime.wednesday, "weekday_letter.wednesday"),
-                      (DateTime.thursday, "weekday_letter.thursday"),
-                      (DateTime.friday, "weekday_letter.friday"),
-                      (DateTime.saturday, "weekday_letter.saturday"),
-                    ])
-                      ButtonSegment(
-                        value: day,
-                        label: Text(context.tr(string)),
-                      ),
-                  ],
-                  selected: weekSelection,
-                  onSelectionChanged: (value) {
+                const SizedBox(width: 10),
+                DropdownMenu(
+                  requestFocusOnTap: false,
+                  initialSelection: frequency,
+                  onSelected: (value) {
                     setState(() {
-                      weekSelection = value;
+                      frequency = value ?? Frequency.daily;
                     });
                   },
-                ),
-              ],
-            // TODO: handle montly on same day and montly
-            // on same weekday
-            _ => [],
-          },
-          Text(
-            context.tr("ends"),
-            style: theme.textTheme.titleSmall,
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Radio(
-                value: _EndOption.never,
-                groupValue: endOption,
-                onChanged: (value) {
-                  setState(() => endOption = value ?? _EndOption.never);
-                },
-              ),
-              Text(context.tr("end_option.never"))
-            ],
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Radio(
-                value: _EndOption.onDate,
-                groupValue: endOption,
-                onChanged: (value) {
-                  setState(() => endOption = value ?? _EndOption.never);
-                },
-              ),
-              Text(context.tr("end_option.on_date")),
-              TextButton(
-                onPressed: endOption != _EndOption.onDate
-                    ? null
-                    : () async {
-                        final date = await showDatePicker(
-                          context: context,
-                          initialDate: endDate ?? DateTime.now(),
-                          firstDate: DateTime(2000),
-                          lastDate: DateTime(2100),
-                        );
-
-                        if (date != null) {
-                          setState(() {
-                            endDate = date;
-                          });
-                        }
-                      },
-                child: Text(switch (endDate) {
-                  final endDate? => DateFormat.yMMMEd(locale).format(endDate),
-                  null => context.tr("select_date"),
-                }),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Radio(
-                value: _EndOption.afterOccurrences,
-                groupValue: endOption,
-                onChanged: (value) {
-                  setState(() => endOption = value ?? _EndOption.never);
-                },
-              ),
-              Text(context.tr("end_option.after_first")),
-              SizedBox(
-                width: 100,
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  controller: limitController,
-                  enabled: endOption == _EndOption.afterOccurrences,
-                  keyboardType: TextInputType.number,
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly,
+                  dropdownMenuEntries: [
+                    DropdownMenuEntry(
+                      value: Frequency.hourly,
+                      label: context.tr("repeat_menu.hour"),
+                    ),
+                    DropdownMenuEntry(
+                      value: Frequency.daily,
+                      label: context.tr("repeat_menu.day"),
+                    ),
+                    DropdownMenuEntry(
+                      value: Frequency.weekly,
+                      label: context.tr("repeat_menu.week"),
+                    ),
+                    DropdownMenuEntry(
+                      value: Frequency.monthly,
+                      label: context.tr("repeat_menu.month"),
+                    ),
+                    DropdownMenuEntry(
+                      value: Frequency.yearly,
+                      label: context.tr("repeat_menu.year"),
+                    ),
                   ],
                 ),
-              ),
-              Text(context.tr("end_option.after_second")),
-            ],
-          ),
-          const SizedBox(height: 15),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text(context.tr("cancel")),
-              ),
-              FilledButton(
-                onPressed: () {
-                  Navigator.of(context).pop(_getRecurrenceRule());
-                },
-                child: Text(context.tr("done")),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+            ...switch (frequency) {
+              Frequency.weekly => [
+                  Text(context.tr("repeat")),
+                  SegmentedButton(
+                    showSelectedIcon: false,
+                    multiSelectionEnabled: true,
+                    expandedInsets: const EdgeInsets.symmetric(vertical: 10),
+                    segments: [
+                      for (final (day, string) in const [
+                        (DateTime.sunday, "weekday_letter.sunday"),
+                        (DateTime.monday, "weekday_letter.monday"),
+                        (DateTime.tuesday, "weekday_letter.tuesday"),
+                        (DateTime.wednesday, "weekday_letter.wednesday"),
+                        (DateTime.thursday, "weekday_letter.thursday"),
+                        (DateTime.friday, "weekday_letter.friday"),
+                        (DateTime.saturday, "weekday_letter.saturday"),
+                      ])
+                        ButtonSegment(
+                          value: day,
+                          label: Text(context.tr(string)),
+                        ),
+                    ],
+                    selected: weekSelection,
+                    onSelectionChanged: (value) {
+                      setState(() {
+                        weekSelection = value;
+                      });
+                    },
+                  ),
+                ],
+              // TODO: handle montly on same day and montly
+              // on same weekday
+              _ => [],
+            },
+            Text(
+              context.tr("ends"),
+              style: theme.textTheme.titleSmall,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Radio(value: _EndOption.never),
+                Text(context.tr("end_option.never"))
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Radio(value: _EndOption.onDate),
+                Text(context.tr("end_option.on_date")),
+                TextButton(
+                  onPressed: endOption != _EndOption.onDate
+                      ? null
+                      : () async {
+                          final date = await showDatePicker(
+                            context: context,
+                            initialDate: endDate ?? DateTime.now(),
+                            firstDate: DateTime(2000),
+                            lastDate: DateTime(2100),
+                          );
+
+                          if (date != null) {
+                            setState(() {
+                              endDate = date;
+                            });
+                          }
+                        },
+                  child: Text(switch (endDate) {
+                    final endDate? => DateFormat.yMMMEd(locale).format(endDate),
+                    null => context.tr("select_date"),
+                  }),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Radio(value: _EndOption.afterOccurrences),
+                Text(context.tr("end_option.after_first")),
+                SizedBox(
+                  width: 100,
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    controller: limitController,
+                    enabled: endOption == _EndOption.afterOccurrences,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                  ),
+                ),
+                Text(context.tr("end_option.after_second")),
+              ],
+            ),
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(context.tr("cancel")),
+                ),
+                FilledButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(_getRecurrenceRule());
+                  },
+                  child: Text(context.tr("done")),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
